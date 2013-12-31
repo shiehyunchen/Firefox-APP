b9a7df578482235ec1bd8723eb7d726b0fbb8ea6
ChihYen Chen <tn01050784@hotmail.com>
2013-12-31 15:45:24 +0800

    add the building IFO. Add the route function. Comment the code.

--- NCU-Student-tool-kit/p-NCU_Map/map.js
+++ NCU-Student-tool-kit/p-NCU_Map/map.js
@@ -1,8 +1,4 @@
 var map;
-//var myMarker;
-//var desMarker;
-var myLatlng;
-var desLatlng;
 var directionsDisplay;
 
 $(document).ready(function () {
@@ -17,99 +13,32 @@ function initializeMap() {
     var options = {
         zoom: 15,
         center: NCU_Center,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+		disableDoubleClickZoom: true,
+		streetViewControl: false
     };
 
     //Display the Map
     map = new google.maps.Map(document.getElementById("NCU_map"), options);
 }
 
-function errorHandler(error) {
-    alert("Error while retrieving current position. Error code: " + error.code + ",Message: " + error.message);
-}
-
-function showMyCurrentLocation(position) {
-    myLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-	findRoute();
-	/*
-	var Name = "Your location";
-    var optionOfMarker = {
-        animation: google.maps.Animation.DROP,
-        position: myLatlng,
-        title: Name
-    };
-	if(myMarker)
-	{
-		myMarker.setMap(null);
-	}
-    myMarker = new google.maps.Marker(optionOfMarker);
-	myMarker.setMap(map);
-	*/
-}
-
-function findMyCurrentLocation() {
-    var geoService = navigator.geolocation;
-    if (geoService) {
-        navigator.geolocation.getCurrentPosition(showMyCurrentLocation, errorHandler, { enableHighAccuracy: true });
-    } else {
-        alert("Your Browser does not support GeoLocation.");
-    }
-}
-
-function showDestination(name,latitude,longitude) {
-	desLatlng = new google.maps.LatLng(latitude, longitude);
-	
-    /*var Name = name;
-    var optionOfMarker = {
-		animation: google.maps.Animation.DROP,
-        position: desLatlng,
-        title: Name
-    };
-	if(desMarker)
-	{
-		desMarker.setMap(null);
-	}
-    desMarker = new google.maps.Marker(optionOfMarker);
-	desMarker.setMap(map);
-	*/
-}
-
-function findDestination(value) {
-	var buildingInfoFile = "./building.json";
-	$.getJSON(buildingInfoFile, function(building){
-		$.each(building, function(i, item){
-			if(item.id == value)
-			{
-				showDestination(item.name,item.latitude,item.longitude);
-				//alert(desLatlng+"\n1");
-				findMyCurrentLocation();
-				
-			}
-		});
-	});
-}
-
-function findRoute(){
+function findRoute(myLatlng,desLatlng){
 	
 	if(directionsDisplay)
 	{
-		//alert(directionsDisplay);
 		directionsDisplay.setMap(null);
 	}
 	directionsDisplay = new google.maps.DirectionsRenderer({
 		'map': map,
-		'preserveViewport': true
+		'preserveViewport': true //dont change the preserveViewport
 	});	
-	//alert(myLatlng+"\n12");
-	var start = myLatlng;
-	//alert(desLatlng+"\n22");
-	var end = desLatlng;
 	
+	//show the text instruction
 	//directionsDisplay.setPanel(document.getElementById("directions_panel"));
 	
 	var request = {
-		origin:start,
-		destination:end,
+		origin: myLatlng,
+		destination: desLatlng,
 		travelMode: google.maps.DirectionsTravelMode.WALKING 
 	};
 	
@@ -122,7 +51,34 @@ function findRoute(){
 	});
 }
 
-function showRoute(value) {
+function findMyCurrentLocation(desLatlng) {
+	//require geoService
+    var geoService = navigator.geolocation;
 	
-	findDestination(value);
+    if (geoService) {
+        navigator.geolocation.getCurrentPosition(
+			//successCallback
+			function(position) {
+				var myLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+				findRoute(myLatlng,desLatlng);
+			}, 
+			//errorCallback
+			function(error) {
+				alert("Error while retrieving current position. Error code: " + error.code + ",Message: " + error.message);
+			});
+    } else {
+        alert("Your Browser does not support GeoLocation.");
+    }
+}
+
+function showRoute(value) {
+	var buildingInfoFile = "./building.json";
+	$.getJSON(buildingInfoFile, function(building){
+		$.each(building, function(i, item){
+			if(item.id == value){
+				var desLatlng = new google.maps.LatLng(item.latitude, item.longitude);
+				findMyCurrentLocation(desLatlng);
+			}
+		});
+	});
 }
